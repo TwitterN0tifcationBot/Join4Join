69df51fc3ff7ab2f0b013ad97e331659b9da6dea
TwitterN0tifcationBot <164622308+TwitterN0tifcationBot@users.noreply.github.com>
2024-06-16 17:31:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ShardingManager } = require('discord.js');
 const { token } = require('./config.json');
 
 const manager = new ShardingManager('./bot.js', {
-    token: token,
+    token: MTI1MTkxNzMzOTE4ODkyMDQxMA.GwwCKa.JjgfYqddRlBOojcp6fzn84cd0xFi5_uOUIHLEI,
     totalShards: 1
 });
 
